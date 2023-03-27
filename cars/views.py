453df19e5afd13083d0b558a068f75610cd95bfd
Kieron Joy-Kimber <kieron.a.joy@gmail.com>
2023-03-27 18:20:27 +0100

    Car lists displaying

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Car
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def car_list(request):
+    cars = Car.objects.all()
+    return render(request, 'car_list.html', {'cars': cars})
